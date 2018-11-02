84bb47e485703c6fd3754ed1908c104de88ba0ab
kcleal <clealk@cardiff.ac.uk>
2018-11-02 13:37:36 +0000

    Updates to calling

---
+++ src/align/pairing.py
@@ -0,0 +1,132 @@
+"""
+Find the best path through a collection of alignments
+Works with paried-end reads rather or single contigs. Borrows the pairing heuristic from bwa.
+"""
+import sys
+
+import click
+import numpy as np
+
+#from align import align_path_c
+from align import align_path_c
+
+
+def read_orientations(t, r1_len, r2_len):
+    """
+    # This code prooves that the orientation of the read dowesnt matter, only the order of the reads:
+    # Permute all orientations, [AB|CD] + AB|DC, BA|DC, BA|CD
+    ab, cd = [t[t[:, 7] == 1], t[t[:, 7] == 2]]
+
+    # Make AB reversed
+    ba = ab.copy()
+    s, e = ba[:, 2].copy(), ba[:, 3].copy()
+    ba[:, 2] = r1_l - e
+    ba[:, 3] = r1_l - s
+    ba = np.flipud(ba)
+
+    # Make CD reversed
+    dc = cd.copy()
+    dc[:, 2:4] -= r1_l
+    s, e = dc[:, 2].copy(), dc[:, 3].copy()
+    dc[:, 2] = r2_l - e
+    dc[:, 3] = r2_l - s
+    dc[:, 2:4] += r1_l
+    dc = np.flipud(dc)
+
+    # Add combos
+    ori.append(np.concatenate([ab, dc]))
+    ori.append(np.concatenate([ba, dc]))
+    ori.append(np.concatenate([ba, cd]))
+
+    These 3 plus the original, produce two sets of identical paths; therefore only the order of reads is important
+    """
+    yield t
+
+    read1_arr, read2_arr = [t[t[:, 7] == 1], t[t[:, 7] == 2]]
+    read2_arr[:, 2:4] -= r1_len
+    read1_arr[:, 2:4] += r2_len
+
+    yield np.concatenate([read2_arr, read1_arr])
+
+
+def process(rt):
+    """
+    Assumes that the reads are ordered read1 then read2, in the FR direction
+    :param rt: Read_template object, contains all parameters within the pairing_params array
+    """
+    r1_len = rt['read1_length']
+    r2_len = rt['read2_length']
+
+    if r2_len is None and r1_len:
+        single_end = True
+        contig_l = r1_len
+    elif r1_len is None and r2_len:
+        single_end = True
+        contig_l = r2_len
+    elif r1_len is None and r2_len is None:
+        return False
+    else:
+        single_end = False
+        contig_l = r1_len + r2_len
+
+    mu, sigma = rt['isize']
+
+    pp = rt["pairing_params"]
+    max_insertion = pp[0]
+    min_aln = pp[1]
+    max_homology = pp[2]
+    ins_cost = pp[3]
+    hom_cost = pp[4]  # 2
+    inter_cost = pp[5]  # 10
+    U = pp[6]
+    match_score = rt["match_score"]
+
+    args = [contig_l, mu, sigma, max_insertion, min_aln, max_homology, ins_cost,
+            hom_cost, inter_cost, U, match_score]
+
+    table = rt['data'][:, range(8)]
+
+    if not single_end:
+        # If it is unclear which read comes first this function can be used to generate both orientations:
+        both_ways = []
+        for r in read_orientations(table, r1_len, r2_len):
+            a_res = align_path_c.optimal_path(r, *args)
+            if len(a_res) > 0:
+                if a_res[1] == a_res[4] and len(a_res[0]) == 2 and a_res[1] - a_res[2] > U:
+                    # Cant do better. Normal pairing with 2 good alignments
+                    return a_res
+            both_ways.append(a_res)
+
+        if len(both_ways) == 0:
+            return False
+        path, length, second_best, dis_to_normal, norm_pairings = sorted(both_ways, key=lambda x: x[1])[-1]  # Best
+
+    else:
+        path, length, second_best, dis_to_normal, norm_pairings = align_path_c.optimal_path(table, *args)
+
+    if int(length) < int(second_best):
+        sys.stderr.write("WARNING: primary path < secondary path\n")
+
+    if rt["name"] == "simulated_reads.0.10-id217_A_chr21:46697360_B_chr6:157282148-28985":
+        click.echo(path, err=True)
+        for idx, row in enumerate(rt["data"].astype(int)):
+
+            click.echo((idx, list(row)), err=True)
+        click.echo(rt["data"].astype(int).tolist(), err=True)
+
+    return path, length, second_best, dis_to_normal, norm_pairings
+
+
+if __name__ == "__main__":
+
+    rt = {"pairing_params": (100, 17, 100, 1, 3, 2, 9), "match_score": 1, "read1_length": 125, "read2_length": 125,
+          "isize": (300, 150), "name": None}
+
+    array = np.array
+
+    rt["data"] = [[1, 46697206, 0, 125, 141, 1, 1, 1, 0], [0, 248943309, 0, 125, 123, 0, 1, 1, 0], [2, 50805563, 0, 125, 123, 2, 1, 1, 0], [3, 113605310, 0, 125, 113, 3, -1, 1, 0], [4, 59802900, 0, 87, 82, 4, -1, 1, 0], [2, 40775679, 0, 87, 82, 5, -1, 1, 0], [5, 43980445, 0, 87, 82, 6, -1, 1, 0], [0, 204171422, 0, 81, 81, 7, -1, 1, 0], [6, 98606459, 0, 81, 81, 8, -1, 1, 0], [7, 94343709, 0, 81, 81, 9, -1, 1, 0], [8, 9134289, 0, 80, 80, 10, -1, 1, 0], [9, 2167926, 0, 81, 76, 11, -1, 1, 0], [3, 187604049, 0, 84, 69, 15, -1, 1, 0], [6, 101163814, 0, 88, 68, 16, -1, 1, 0], [10, 18096396, 0, 83, 68, 17, -1, 1, 0], [5, 64586379, 0, 88, 68, 19, -1, 1, 0], [7, 73759684, 0, 89, 64, 23, -1, 1, 0], [5, 142667073, 0, 88, 63, 24, -1, 1, 0], [13, 45671280, 0, 90, 60, 26, -1, 1, 0], [15, 373402, 0, 90, 60, 27, -1, 1, 0], [0, 40454613, 0, 90, 50, 31, -1, 1, 0], [13, 1201895, 2, 86, 67, 20, -1, 1, 0], [13, 8609030, 3, 94, 53, 30, -1, 1, 0], [8, 22647563, 6, 93, 66, 22, -1, 1, 0], [11, 187313656, 9, 93, 75, 13, -1, 1, 0], [12, 122814974, 9, 96, 68, 18, -1, 1, 0], [14, 50877744, 37, 139, 41, 36, 1, 1, 0], [11, 39676689, 38, 163, 42, 35, 1, 1, 0], [10, 152311907, 39, 164, 76, 12, 1, 1, 0], [10, 125458533, 39, 164, 71, 14, 1, 1, 0], [11, 101529556, 39, 164, 66, 21, 1, 1, 0], [10, 186980476, 39, 164, 46, 34, 1, 1, 0], [14, 70721343, 40, 165, 60, 25, 1, 1, 0], [16, 50737539, 41, 154, 57, 29, 1, 1, 0], [0, 213916185, 41, 166, 49, 32, 1, 1, 0], [12, 154777980, 41, 166, 49, 33, 1, 1, 0], [7, 38079164, 42, 167, 58, 28, 1, 1, 0], [16, 248081, 84, 209, 34, 37, -1, 1, 0], [17, 207874, 84, 206, 31, 38, -1, 1, 0], [18, 157282298, 125, 250, 120, 39, -1, 2, 0], [5, 18811230, 125, 237, 82, 41, -1, 2, 0], [8, 90164704, 125, 237, 74, 53, -1, 2, 0], [10, 125390819, 125, 223, 72, 55, -1, 2, 0], [18, 161490568, 125, 237, 72, 56, -1, 2, 0], [7, 96000479, 125, 223, 70, 60, -1, 2, 0], [16, 21838577, 125, 223, 70, 62, -1, 2, 0], [16, 7128391, 125, 213, 70, 63, -1, 2, 0], [12, 150478530, 125, 227, 70, 65, -1, 2, 0], [13, 45135143, 125, 237, 69, 66, -1, 2, 0], [10, 128017277, 125, 238, 68, 68, -1, 2, 0], [6, 86259045, 125, 227, 67, 69, -1, 2, 0], [10, 138550540, 125, 237, 67, 70, -1, 2, 0], [4, 63004434, 125, 243, 65, 71, -1, 2, 0], [7, 95184760, 125, 250, 63, 73, -1, 2, 0], [16, 58101697, 125, 250, 62, 76, -1, 2, 0], [14, 83084687, 125, 249, 61, 79, -1, 2, 0], [3, 84943598, 125, 221, 61, 80, 1, 2, 0], [23, 75905083, 125, 240, 47, 87, -1, 2, 0], [0, 8142172, 125, 240, 42, 91, -1, 2, 0], [0, 211533467, 125, 228, 41, 94, 1, 2, 0], [23, 104771997, 125, 240, 32, 101, -1, 2, 0], [25, 54275176, 125, 167, 28, 103, 1, 2, 0], [0, 145454989, 125, 156, 26, 104, 1, 2, 0], [19, 67803368, 126, 251, 74, 52, 1, 2, 0], [9, 3312166, 126, 245, 68, 67, -1, 2, 0], [23, 87274832, 126, 229, 62, 78, 1, 2, 0], [4, 110688626, 126, 227, 33, 100, 1, 2, 0], [2, 30540737, 127, 252, 56, 83, 1, 2, 0], [6, 129916366, 127, 199, 42, 93, 1, 2, 0], [23, 133403648, 127, 182, 39, 96, 1, 2, 0], [7, 14599999, 127, 176, 34, 98, 1, 2, 0], [7, 22011814, 128, 177, 36, 97, 1, 2, 0], [16, 42264889, 129, 241, 78, 42, -1, 2, 0], [3, 172180973, 129, 244, 58, 81, -1, 2, 0], [0, 51028083, 130, 255, 47, 85, 1, 2, 0], [18, 107283755, 131, 256, 64, 72, 1, 2, 0], [3, 60447349, 131, 234, 62, 77, 1, 2, 0], [24, 84295617, 131, 234, 42, 92, 1, 2, 0], [19, 4912827, 132, 257, 85, 40, 1, 2, 0], [11, 155876992, 135, 238, 63, 74, 1, 2, 0], [22, 41615, 135, 238, 63, 75, 1, 2, 0], [11, 24058675, 137, 262, 51, 84, 1, 2, 0], [2, 41921621, 138, 263, 77, 43, 1, 2, 0], [12, 124858366, 138, 263, 77, 44, 1, 2, 0], [0, 212470930, 138, 241, 75, 46, 1, 2, 0], [3, 229960838, 138, 263, 75, 50, 1, 2, 0], [17, 28968903, 138, 263, 75, 51, 1, 2, 0], [3, 235293681, 138, 260, 72, 54, 1, 2, 0], [12, 135068333, 138, 263, 71, 58, 1, 2, 0], [21, 57130108, 138, 263, 70, 61, 1, 2, 0], [24, 77976321, 139, 262, 47, 86, -1, 2, 0], [14, 30664946, 147, 259, 75, 47, -1, 2, 0], [20, 2950483, 147, 259, 75, 48, -1, 2, 0], [19, 8007483, 147, 259, 75, 49, -1, 2, 0], [17, 119545508, 147, 260, 71, 59, -1, 2, 0], [3, 33474502, 147, 269, 70, 64, -1, 2, 0], [21, 22859021, 147, 271, 45, 88, -1, 2, 0], [11, 71159563, 147, 269, 45, 89, -1, 2, 0], [11, 64836968, 147, 271, 44, 90, -1, 2, 0], [8, 94128350, 147, 272, 41, 95, -1, 2, 0], [5, 48635056, 148, 273, 76, 45, 1, 2, 0], [5, 136846542, 151, 270, 57, 82, -1, 2, 0], [0, 183153560, 151, 275, 33, 99, -1, 2, 0], [3, 169663932, 152, 277, 71, 57, 1, 2, 0], [13, 28412837, 201, 326, 29, 102, -1, 2, 0]]
+    rt["data"] = np.array(rt["data"]).astype(float)
+
+    print(process(rt))
+
+
---
+++ src/align/samclips.py
@@ -0,0 +1,363 @@
+"""
+Utils to generate proper sam output and flag information
+"""
+
+import re
+import click
+
+
+def echo(*arg):
+    click.echo(arg, err=True)
+
+
+def rev_comp(s):
+    r = {"A": "T", "C": "G", "G": "C", "T": "A", "N": "N", "a": "T", "c": "G", "g": "C", "t": "A", "n": "N"}
+    return "".join(r[i] for i in s)[::-1]
+
+
+def set_bit(v, index, x):
+    """Set the index:th bit of v to 1 if x is truthy, else to 0, and return the new value."""
+    mask = 1 << index  # Compute mask, an integer with just bit 'index' set.
+    v &= ~mask  # Clear the bit indicated by the mask (if x is False)
+    if x:
+        v |= mask  # If x was True, set the bit indicated by the mask.
+    return v
+
+
+def add_softclips(s, fq, readid):
+    fq_seq = fq[readid][0].upper()
+    fq_qual = fq[readid][1]
+    if s.flag & 16:
+        fq_seq = rev_comp(fq_seq)
+        fq_qual = fq_qual[::-1]
+    s.seq = fq_seq
+    s.qual = fq_qual
+
+
+def set_mate_flag(a, b, r1_len, r2_len, max_d, read1_rev, read2_rev, template):
+
+    if not a or not b:  # No alignment, mate unmapped?
+        return False, False
+
+    # Make sure chromosome of mate is properly set not "*"
+    chrom_a, mate_a = a[2], a[5]
+    chrom_b, mate_b = b[2], b[5]
+    if chrom_a != mate_b:
+        b[5] = chrom_a
+    if chrom_b != mate_a:
+        a[5] = chrom_b
+
+    aflag = int(a[0])
+    bflag = int(b[0])
+
+    reverse_A = False
+    reverse_B = False
+
+    # If set as not primary, and has been aligned to reverse strand, and primary is mapped on forward
+    # the sequence needs to be rev complement
+    if aflag & 256:
+        if (aflag & 16) and (not read1_rev):
+            reverse_A = True
+        elif (not aflag & 16) and read1_rev:
+            reverse_A = True
+
+    if bflag & 256:
+        if (bflag & 16) and (not read2_rev):
+            reverse_B = True
+        elif (not bflag & 16) and read2_rev:
+            reverse_B = True
+
+    # Turn off secondary flag as these are primary pair
+    aflag = set_bit(aflag, 8, 0)
+    bflag = set_bit(bflag, 8, 0)
+
+    # Turn off proper pair flag, might be erroneously set
+    aflag = set_bit(aflag, 1, 0)
+    bflag = set_bit(bflag, 1, 0)
+
+    # Set paired
+    aflag = set_bit(aflag, 0, 1)
+    bflag = set_bit(bflag, 0, 1)
+
+    # Set first and second in pair, in case not set
+    aflag = set_bit(aflag, 6, 1)
+    bflag = set_bit(bflag, 7, 1)
+
+    # Turn off any mate reverse flags, these should be reset
+    aflag = set_bit(aflag, 5, 0)
+    bflag = set_bit(bflag, 5, 0)
+
+    # If either read is unmapped
+    if aflag & 4:
+        bflag = set_bit(bflag, 3, 1)  # Position 3, change to 1
+    if bflag & 4:
+        aflag = set_bit(aflag, 3, 1)
+
+    # If either read on reverse strand
+    if aflag & 16:
+        bflag = set_bit(bflag, 5, 1)
+    if bflag & 16:
+        aflag = set_bit(aflag, 5, 1)
+
+    # Set unmapped
+    arname = a[1]
+    apos = a[2]
+    if apos == "0":  # -1 means unmapped
+        aflag = set_bit(aflag, 2, 1)
+        bflag = set_bit(bflag, 8, 1)
+
+    brname = b[1]
+    bpos = b[2]
+    if b[2] == "0":
+        bflag = set_bit(bflag, 2, 1)
+        aflag = set_bit(aflag, 8, 1)
+
+    # Set RNEXT and PNEXT
+    a[5] = brname
+    a[6] = bpos
+
+    b[5] = arname
+    b[6] = apos
+
+    if not (apos == "-1" or bpos == "-1"):
+
+        if arname == brname:
+            # Set TLEN
+            p1, p2 = int(apos), int(bpos)
+            if p1 < p2:
+                tl = p2 + r2_len - p1
+                a[7] = str(tl)
+                b[7] = str(-1 * tl)
+            else:
+                tl = p2 - p1 - r1_len
+                a[7] = str(tl)
+                b[7] = str(-1 * tl)
+
+            # Set proper-pair flag
+            if (aflag & 16 and not bflag & 16) or (not aflag & 16 and bflag & 16):  # Not on same strand
+
+                if abs(p1 - p2) < max_d:
+                    # Check for FR or RF orientation
+                    if (p1 < p2 and (not aflag & 16) and (bflag & 16)) or (p2 <= p1 and (not bflag & 16) and (aflag & 16)):
+                        aflag = set_bit(aflag, 1, 1)
+                        bflag = set_bit(bflag, 1, 1)
+
+                        # If proper pair, sometimes the mate-reverse-strand flag is set
+                        # this subsequently means the sequence should be reverse complemented!
+                        if aflag & 16 and not bflag & 32:
+                            # Mate-reverse strand not set
+                            bflag = set_bit(bflag, 5, 1)
+                            # reverse_B = True
+
+                        if not aflag & 16 and bflag & 32:
+                            # Mate-reverse should'nt be set
+                            bflag = set_bit(bflag, 5, 0)
+                            reverse_A = True
+
+                        if bflag & 16 and not aflag & 32:
+                            # Mate-reverse strand not set
+                            aflag = set_bit(aflag, 5, 1)
+                            # reverse_A = True
+
+                        if not bflag & 16 and aflag & 32:
+                            # Mate-revsere should'nt be set
+                            aflag = set_bit(aflag, 5, 0)
+                            reverse_B = True
+
+    a[0] = str(aflag)
+    b[0] = str(bflag)
+    return reverse_A, reverse_B
+
+
+def set_supp_flags(sup, pri, primary_reversed, template):
+    # Set paired
+    supflag = int(sup[0])
+    priflag = int(pri[0])
+
+    # Set supplementary flag
+    if not supflag & 1:
+        supflag = set_bit(supflag, 0, 1)
+
+    # If primary is on reverse strand, set the mate reverse strand tag
+    if priflag & 16 and not supflag & 32:
+        supflag = set_bit(supflag, 5, 1)
+
+    # Turn off not-primary-alignment
+    if supflag & 256:
+        supflag = set_bit(supflag, 8, 0)
+
+    # Sometimes supplementary needs to be reverse complemented too
+    # Occurs when primary has been rev-comp and supp is forward strand or vice versa
+    # if primary_reversed and not supflag & 16:
+    #     rev_sup = True
+    if not primary_reversed and supflag & 16:
+        rev_sup = True
+    else:
+        rev_sup = False
+
+    sup[0] = str(supflag)
+
+    sup[5] = pri[1]
+    sup[6] = pri[2]
+    return rev_sup
+
+
+def add_sequence_back(atype, item, reverse_me, template):
+
+    c = re.split(r'(\d+)', item[4])[1:]  # Drop leading empty string
+    start = 0
+
+    cigar_length = sum([int(c[i]) for i in range(0, len(c), 2) if c[i + 1] not in "DH"])
+    flag = int(item[0])
+
+    if flag & 64 and template["read1_seq"]:
+        name = "read1"
+        if template["fq_read1_seq"] is not None:
+            end = len(template["fq_read1_seq"])
+        else:
+            end = len(template["read1_seq"])
+
+    elif flag & 128 and template["read2_seq"]:
+        name = "read2"
+        if template["fq_read2_seq"] is not None:
+            end = len(template["fq_read2_seq"])
+        else:
+            end = len(template["read2_seq"])
+    else:
+        return  # read sequence is None or bad flag
+
+    # Try and replace H with S
+    if c[1] == "H" or c[-1] == "H":
+        # Replace hard with soft-clips
+        non_hardclipped_length = sum([int(c[i]) for i in range(0, len(c), 2) if c[i + 1] not in "D"])
+        if non_hardclipped_length == end:
+            item[4] = item[4].replace("H", "S")
+            cigar_length = non_hardclipped_length
+        else:
+            # Remove seq
+            if c[1] == "H":
+                start += int(c[0])
+            if c[-1] == "H":
+                end -= int(c[-2])
+
+    if item[9] == "*":
+        if reverse_me:
+            item[8] = rev_comp(template["%s_seq" % name])[start:end]
+            item[9] = template["%s_q" % name][::-1][start:end]
+        else:
+            item[8] = template["%s_seq" % name][start:end]
+            item[9] = template["%s_q" % name][start:end]
+
+    elif item[9] in template["fq_%s_q" % name]:
+        item[8] = template["fq_%s_seq" % name][start:end]
+        item[9] = template["fq_%s_q" % name][start:end]
+
+    elif item[9] in template["fq_%s_q" % name][::-1]:
+        item[8] = rev_comp(template["fq_%s_seq" % name])[start:end]
+        item[9] = template["fq_%s_q" % name][::-1][start:end]
+    else:
+        echo("---")
+        echo(item[9], flag)
+        echo(name)
+        echo(template["read1_q"])
+        echo(template["read2_q"])
+        echo(item)
+        echo(rev_comp(template["fq_%s_seq" % name]))
+        raise ValueError
+
+    assert len(item[8]) == cigar_length
+
+
+def fixsam(template):
+
+    sam = [template['inputdata'][i] for i in template['rows']]
+    r1l, r2l = template["read1_length"], template["read2_length"]
+    max_d = template['max_d']
+
+    # Todo make sure all read-pairs have a mapping, otherwise write an unmapped
+
+    paired = False if template["read2_length"] is None else True
+    score_mat = template['score_mat']
+
+    out = []
+    primary1 = None
+    primary2 = None
+    rev_A = False
+    rev_B = False
+    for l in sam:
+        t = score_mat
+        strand = "-1" if int(l[0]) & 16 else "1"
+        rid = 2 if int(l[0]) & 128 else 1
+        key = "{}-{}-{}-{}".format(l[1], str(int(l[2])), strand, rid)
+
+        if len(t[key]) > 2:
+            # Prevent bug where two identical alignments possible
+            aln_info_0, aln_info_1 = t[key].pop(0), t[key].pop(0)  # Remove first two items from list
+        else:
+            aln_info_0, aln_info_1 = t[key]
+
+        if not aln_info_0:  # Not primary, set supplementary flag
+            l[0] = str(set_bit(int(l[0]), 11, 1))
+
+        if rid == 1:
+            if t["splitter"][0]:
+                split = "1"
+            else:
+                split = "0"
+        elif rid == 2:
+            if t["splitter"][1]:
+                split = "1"
+            else:
+                split = "0"
+        # "@RG\tID:0\tSM:0\tPU:0\tPL:0\tLB:0\n"
+        xs = int(aln_info_1)
+
+        l += ["SP:Z:" + split,
+              "DA:i:" + str(xs),
+              "DP:Z:" + str(round(t["dis_to_next_path"], 0)),
+              "DN:Z:" + str(round(t["dis_to_normal"], 2)),
+              "PS:Z:" + str(round(t["path_score"], 2)),
+              "NP:Z:" + str(round(t["normal_pairings"], 1)),
+              "RG:Z:0", "SM:Z:0", "PU:Z:lane1", "PL:Z:ILLUMINA", "LB:Z:0",
+              ]
+
+        if aln_info_0:
+            if rid == 1:
+                primary1 = l
+            else:
+                primary2 = l
+        else:
+            out.append(['sup', l, False])  # Supplementary
+
+    if primary1 is None or primary2 is None:
+        return []  # Todo deal with unmapped read or unpaired
+
+    if paired:
+
+        rev_A, rev_B = set_mate_flag(primary1, primary2, r1l, r2l, max_d,
+                                     template["read1_reverse"], template["read2_reverse"], template)
+
+        # Check if supplementary needs reverse complementing
+        for i in range(len(out)):
+            if int(out[i][1][0]) & 64:  # Flag
+                revsup = set_supp_flags(out[i][1], primary1, template["read1_reverse"], template)
+            else:
+                revsup = set_supp_flags(out[i][1], primary2, template["read2_reverse"], template)
+            if revsup:
+                out[i][2] = True
+
+    out = [('pri', primary1, rev_A), ('pri', primary2, rev_B)] + out
+
+    # Add read seq info back in if necessary, before reverse complementing. Check for hard clips and clip as necessary
+    for a_type, item, reverse_me in out:
+        if item:
+            if len(item[8]) <= 1 or "H" in item[4]:  # Sequence is set as "*", needs adding back in, or hard-clipped
+                add_sequence_back(a_type, item, reverse_me, template)
+            elif reverse_me:
+                item[8] = rev_comp(item[8])
+                item[9] = item[9][::-1]
+        else:
+            # None here means no alignment for primary2
+            pass
+
+    return [i[1] for i in out if i[1] is not None]
